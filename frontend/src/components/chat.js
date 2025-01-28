72cb71f347a4b4ffc0cb0639718b36d6989fb2d0
srilaya2002 <evtcsrilaya@gmail.com>
2025-01-28 18:38:15 +0300

    test commit

--- frontend/src/components/chat.js
+++ frontend/src/components/chat.js
@@ -3,7 +3,6 @@ import {
     MainContainer,
     ChatContainer,
     MessageList,
-    Message,
     MessageInput,
 } from '@chatscope/chat-ui-kit-react';
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
